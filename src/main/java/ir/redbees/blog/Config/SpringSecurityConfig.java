b9590f62ca6ec24d93a31be9175a2a6bfefea230
MahdiKheirmand <46438288+MahdiKheirmand@users.noreply.github.com>
2020-03-16 08:47:18 +0330

    move create_account index.html -> login.html

--- src/main/java/ir/redbees/blog/Config/SpringSecurityConfig.java
+++ src/main/java/ir/redbees/blog/Config/SpringSecurityConfig.java
@@ -1,20 +1,21 @@
 package ir.redbees.blog.Config;
 
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 
 import javax.sql.DataSource;
 
 @Configuration
 public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
-    @Autowired
-    private DataSource dataSource;
+    private final DataSource dataSource;
+
+    public SpringSecurityConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
